fcec6ffb91f5739e9890735d48e542ce866a3110
Raymond Orlana <rayorlana@gmail.com>
2023-05-25 09:21:59 +0700

    feat: autofocus mgmt & improve accessibility

--- components/AddEditEventModal.tsx
+++ components/AddEditEventModal.tsx
@@ -24,7 +24,7 @@ const AddEditEventModal = ({ onClose, data, onSave, onDelete }: AddEditEventModa
   const [eventName, setEventName] = useState(data?.name ?? '');
   const [eventTime, setEventTime] = useState<SelectOption | null>(data?.date ? determineInitialEventTime(new Date(data?.date)) : null);
   const [eventInviteesEmail, setEventInviteesEmail] = useState(data?.invitees?.length ? data?.invitees.join() : '');
-  const dialogRef = useRef<HTMLElement | null>(null);
+  const focusRef = useRef<HTMLInputElement | null>(null);
 
   const timeOptions = useMemo(() => getEventTimeOptions(), []);
 
@@ -32,6 +32,7 @@ const AddEditEventModal = ({ onClose, data, onSave, onDelete }: AddEditEventModa
     <div className="flex items-center">
       <label className="mr-5 w-14 font-medium text-primary">Name</label>
       <input
+        ref={focusRef}
         className="flex-1 border-b border-b-gray-400/60 p-1.5 pl-0 text-sm outline-none transition-all duration-150 hover:bg-gray-200/50 focus:border-b-primary focus:bg-transparent"
         placeholder="Event name / title"
         value={eventName}
@@ -65,7 +66,7 @@ const AddEditEventModal = ({ onClose, data, onSave, onDelete }: AddEditEventModa
   const renderModalBody = () => {
     const isDisabled = !eventName;
     return (
-      <div className="mt-7 flex flex-col gap-y-2.5">
+      <form className="mt-7 flex flex-col gap-y-2.5">
         {renderEventNameInput()}
         {renderEventDatetimeInput()}
         {renderEventInviteesInput()}
@@ -73,6 +74,7 @@ const AddEditEventModal = ({ onClose, data, onSave, onDelete }: AddEditEventModa
           <div>
             {!!data?.id && (
               <button
+                type="button"
                 className={clsxm(
                   'w-[100px] rounded bg-red-500 px-4 py-1.5 text-center text-sm font-medium uppercase text-white shadow-md transition-all duration-150 hover:opacity-90',
                   {
@@ -87,6 +89,7 @@ const AddEditEventModal = ({ onClose, data, onSave, onDelete }: AddEditEventModa
             )}
           </div>
           <button
+            type="submit"
             className={clsxm(
               'w-[100px] rounded bg-primary px-4 py-1.5 text-center text-sm font-medium uppercase text-white shadow-md transition-all duration-150 hover:opacity-90',
               {
@@ -99,25 +102,22 @@ const AddEditEventModal = ({ onClose, data, onSave, onDelete }: AddEditEventModa
             Save
           </button>
         </div>
-      </div>
+      </form>
     );
   };
   return (
-    <Dialog open={true} onClose={onClose} className="relative z-40" initialFocus={dialogRef}>
+    <Dialog open={true} onClose={onClose} className="relative z-40" initialFocus={focusRef}>
       {/* Backdrop */}
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
       <div className="fixed inset-0 flex items-center justify-center p-4">
         <Dialog.Panel className="relative mx-auto w-full max-w-xl rounded-3xl bg-white p-12 pb-7">
-          {/* @ts-expect-errors any dialogRef */}
-          <div ref={dialogRef}>
-            <Dialog.Title className="text-green absolute left-12 top-5 text-left text-2xl font-bold uppercase tracking-wide text-primary">
-              {data?.id ? 'Edit Event' : 'Add Event'}
-            </Dialog.Title>
-            <button className="absolute right-12 top-5" onClick={onClose}>
-              <IconClose className="h-4 w-4" />
-            </button>
-            {renderModalBody()}
-          </div>
+          <Dialog.Title className="text-green absolute left-12 top-5 text-left text-2xl font-bold uppercase tracking-wide text-primary">
+            {data?.id ? 'Edit Event' : 'Add Event'}
+          </Dialog.Title>
+          <button className="absolute right-12 top-5" onClick={onClose}>
+            <IconClose className="h-4 w-4" />
+          </button>
+          {renderModalBody()}
         </Dialog.Panel>
       </div>
     </Dialog>
